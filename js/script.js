2835f5956dca555655e8353d13698121cc4d3bcf
Dmytro Hapchyn <48380610+Sseik@users.noreply.github.com>
2025-11-14 12:59:50 +0200

    Add coloring squares in black when hovered over and erasing when hovered with ctrl key held

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ for (let i = 0; i < 16 * 16; i++) {
     squareDiv.style.height = squareDiv.style.width;
     container.appendChild(squareDiv);
 }
+
+container.addEventListener('mouseover', (e) => {
+    if(e.target.id) return;
+    if (e.ctrlKey) e.target.style.backgroundColor = '';
+    else e.target.style.backgroundColor = 'black';
+})
